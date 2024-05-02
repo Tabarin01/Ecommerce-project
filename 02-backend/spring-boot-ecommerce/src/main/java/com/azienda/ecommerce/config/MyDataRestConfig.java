a693f47aac8632b40ecd8758f2fbf910f828eb7a
Tabarin01 <sandrodesilvestro@gmail.com>
2024-05-02 11:44:40 +0200

    Back-end entity and HTTP disable put, post and delete

---
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/azienda/ecommerce/config/MyDataRestConfig.java
@@ -0,0 +1,34 @@
+package com.azienda.ecommerce.config;
+
+
+import com.azienda.ecommerce.entity.Product;
+import com.azienda.ecommerce.entity.ProductCategory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.http.HttpMethod;
+
+@Configuration
+public class MyDataRestConfig implements RepositoryRestConfigurer {
+
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+        RepositoryRestConfigurer.super.configureRepositoryRestConfiguration(config, cors);
+
+        HttpMethod[] theUnsupportedActions = {HttpMethod.PUT, HttpMethod.POST, HttpMethod.DELETE};
+
+        //disable http methods for Product: put, post and delete
+        config.getExposureConfiguration()
+                .forDomainType(Product.class)
+                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
+                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+
+        //disable http methods for ProductCategory: put, post and delete
+        config.getExposureConfiguration()
+                .forDomainType(ProductCategory.class)
+                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
+                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+
+    }
+}
---
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/azienda/ecommerce/dao/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.azienda.ecommerce.dao;
+
+import com.azienda.ecommerce.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@CrossOrigin("http://localhost:4200")
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
